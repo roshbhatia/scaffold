f7b21b8ffea7dd1b5f5feb2e1c2b1648eabd6d27
Roshan Bhatia <roshan@timebyping.com>
2023-05-25 13:37:22 -0700

    get k8s deployments working

--- workflows/std/shared/workflows.go
+++ workflows/std/shared/workflows.go
@@ -2,18 +2,65 @@ package shared
 
 import (
 	"context"
+	"os"
 	"time"
 
 	"go.temporal.io/sdk/workflow"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
+func DeployKubernetes(ctx context.Context, deploymentName string, imageName string, replicas int32) error {
+	kubeconfig := os.Getenv("KUBECONFIG")
+	loadingRules := &clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeconfig}
+	config := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, &clientcmd.ConfigOverrides{})
+	restConfig, err := config.ClientConfig()
+	if err != nil {
+		return err
+	}
 
-func DeployKubernetes(ctx context.Context, deploymentName string, imageName string, replicas int) error {
-	// Kubernetes deployment logic goes here.
-	// Use the kubernetes client-go library to interact with your Kubernetes cluster.
-	// You might fetch the KUBECONFIG environment variable here to configure your client.
+	clientset, err := kubernetes.NewForConfig(restConfig)
+	if err != nil {
+		return err
+	}
 
-	// Return nil if the deployment is successful, or an error if not.
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: deploymentName,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": deploymentName,
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": deploymentName,
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  deploymentName,
+							Image: imageName,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	_, err = clientset.AppsV1().Deployments("default").Create(ctx, deployment, metav1.CreateOptions{})
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
